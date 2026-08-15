5fcecb5dd7783e39e8f00e9565b3294572d531bd
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-08-15 00:37:39 -0600

    chore: update segment prefab

--- Assets/Scripts/SegmentPool.cs
+++ Assets/Scripts/SegmentPool.cs
@@ -13,7 +13,7 @@ public class SegmentPool : MonoBehaviour
 
     private Queue<GameObject> activeSegments = new Queue<GameObject>();
 
-    private float segmentLength = 40f;
+    private float segmentLength = 80f;
     private float spawnZ = 0f;
 
 
