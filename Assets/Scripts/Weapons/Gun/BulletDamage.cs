a5e661dd3ab5cba0031ad338b9ada2adfcac2066
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-08-18 00:37:48 -0600

    chore: update enemies in danniels scene

---
+++ Assets/Scripts/Weapons/Gun/BulletDamage.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    [SerializeField] private int damage = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyDanniel enemy =
+            other.GetComponent<EnemyDanniel>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+    }
+}
